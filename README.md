68152fe9b76fcef46f79b286eed58ba974c6ef71
mayasundar <64454649+mayasundar@users.noreply.github.com>
2024-02-23 18:09:09 -0500

    first commit

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-# atproto starter kit
+# bluesky met museum art bot
 
-Use this as a template repo. Requires Node 18+ with typescript and ts-node installed globally (`npm i -g typescript ts-node`). Add your bluesky username and password to `.env` then run your app with `npm run start` or `ts-node-esm index.ts`.
+This bot posts artwork from the Metropolitan Museum of Art every 30 minutes to metmuseumbot.bsky.social
 
-I recommend using `eslint` (for linting) and `prettier` (for enforcing code style) in your chosen text editor (or from the command line).
-
-API documentation can be found here: [https://github.com/bluesky-social/atproto/tree/main/packages/api](https://github.com/bluesky-social/atproto/tree/main/packages/api)
-
-@ me on bluesky (@aliceisjustplaying.bsky.social, did:plc:by3jhwdqgbtrcc7q4tkkv3cf) if you've built something interesting with it!
+Used atproto template from @aliceisjustplaying! Thank you! 😊
--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ const { BskyAgent } = bsky;
 import * as dotenv from 'dotenv';
 import process from 'node:process';
 dotenv.config();
+import fs from 'fs';
 
 const agent = new BskyAgent({
   service: 'https://bsky.social',
@@ -13,26 +14,63 @@ await agent.login({
   password: process.env.BSKY_PASSWORD!,
 });
 
-const bleet = 'You can find the code for this bleet >>>here<<<, with a link card, a title and a description!';
-await agent.post({
-  text: bleet,
-  facets: [
-    {
-      index: { byteStart: bleet.indexOf('>>>') + 3, byteEnd: bleet.indexOf('<<<') },
-      features: [
-        {
-          $type: 'app.bsky.richtext.facet#link',
-          uri: 'https://github.com/aliceisjustplaying/atproto-starter-kit',
+const POLLING_INTERVAL= 1800000;
+const API_ENDPOINT = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=painting';
+const POSTED = 'posted.txt'
+async function getImg(): Promise<void>{
+  try{
+    const response = await fetch(API_ENDPOINT);
+    if (response.ok){
+      const data = await response.json();
+      // get a random index from the array of object IDs
+      const postedIDs = new Set(fs.readFileSync(POSTED, 'utf8').split('\n'));      
+      const randomIndex = Math.floor(Math.random() * data.objectIDs.length);
+      const randomID = data.objectIDs[randomIndex];
+      if (!postedIDs.has(randomID.toString())) {
+        // fetch object response (has media, artist + art data)
+        const objResponse = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomID}`);
+        if (objResponse.ok) {
+          const objData = await objResponse.json();
+          const {primaryImageSmall, artistDisplayName, title, medium, objectDate, objectURL} = objData;
+          // don't allow unknown/anonymous artists, or objects with no small image
+          if (primaryImageSmall && primaryImageSmall !== "" && artistDisplayName && artistDisplayName !== "" && artistDisplayName !== "anonymous" && artistDisplayName !== "unknown") {
+            const postDescription = `${artistDisplayName}, ${title}, ${medium}, ${objectDate} \n`;
+              
+            // convert image to blob
+            const res = await fetch(primaryImageSmall);
+            if (res.ok) {
+              const thumb = await res.blob();
+              postedIDs.add(randomID.toString());
+              fs.appendFileSync(POSTED, `${randomID}\n`);
+
+              const uploadRes = await fetch("https://bsky.social/xrpc/com.atproto.repo.uploadBlob", {
+                method: "POST",
+                body: thumb,
+                headers: {
+                  "Content-Type": "image/jpeg",
+                  Authorization: `Bearer ${agent.session!.accessJwt}`,
+                },
+              });
+
+              const {blob} = await uploadRes.json();
+
+              // finally, make the post
+              await agent.post({
+                text: postDescription,
+
+                embed: {
+                  $type: 'app.bsky.embed.images',
+                  images: [{image: blob, alt: postDescription}],
+                },
+                });
+            }
+          }
         }
-      ]
+      }
     }
-  ],
-  embed: {
-    $type: 'app.bsky.embed.external',
-    external: {
-      uri: 'https://github.com/aliceisjustplaying/atproto-starter-kit',
-      title: "alice's atproto starter kit",
-      description: "i'm just playing around with the api",
-    },
-  },
-});
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+  setTimeout(getImg, POLLING_INTERVAL);
+}
+getImg();
